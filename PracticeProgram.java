d9b50806c0caad474dff81cffadadaf44e140486
samridhi singh <105384370+samridhi1901@users.noreply.github.com>
2025-01-18 15:27:34 +0530

    Add files via upload

---
+++ PracticeProgram.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class even{
+public static void main(String[] args){
+System.out.println("even number between 1 to 10");
+for(int i=1;i<=10;i++){
+if(i%2 == 0){
+System.out.print(i);
+}
+}
+}
+}
